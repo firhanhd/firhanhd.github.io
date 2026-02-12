4d4752f26705f3063303a835079f36a6bcf94c8c
FirhanHD <firhanhadi.1998@gmail.com>
2026-02-12 23:44:12 +0700

    Fix comment syntax in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-Elements
+//Elements
 const envelope = document.getElementById("envelope-container");
 const letter = document.getElementById("letter-container");
 const noBtn = document.querySelector(".no-btn");
